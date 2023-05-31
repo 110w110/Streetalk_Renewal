b72e3e69a81bfdaac87cfca453a6806f41cd5027
110w110 <110w110.official@gmail.com>
2023-05-31 17:52:25 +0900

    APIService 추가 (네트워크 모듈)

---
+++ Streetalk2.0/APIService/APIService.swift
@@ -0,0 +1,17 @@
+//
+//  APIService.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/05/31.
+//
+
+import Foundation
+
+final class APIService {
+    static let shared = APIService()
+    private init() {}
+    
+    func request() {
+        
+    }
+}
