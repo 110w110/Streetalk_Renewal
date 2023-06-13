e2222392fea2ffe314f52158d7a9250f9946a3fa
110w110 <110w110.official@gmail.com>
2023-06-13 15:31:22 +0900

    UIColor extension을 통해 streetalkPink와 streetalkOrange 구현

---
+++ Streetalk2.0/Extension/UIColor+.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/06/13.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    public class var streetalkPink : UIColor {
+        return UIColor(red: 197.0 / 255.0, green: 56.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+    }
+    
+    public class var streetalkOrange : UIColor {
+        return UIColor(red: 232.0 / 255.0, green: 139.0 / 255.0, blue: 0.0, alpha: 1.0)
+    }
+    
+}
