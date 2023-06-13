a4b970465f0401927a510db5aef1e86b83014776
110w110 <110w110.official@gmail.com>
2023-06-13 20:26:09 +0900

    STCollectionView에서 Stretchable Header View 구현을 위한 코드 작성

---
+++ Streetalk2.0/Resoucres/STCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  STCollectionReusableView.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/06/13.
+//
+
+import UIKit
+
+class STCollectionReusableView: UICollectionReusableView {
+        
+}
