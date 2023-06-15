c3e4d2e25f95e07ea1d101c8a00cc38fc31a863e
110w110 <110w110.official@gmail.com>
2023-06-15 23:15:15 +0900

    STTextView 구현으로 글쓰기 뷰컨트롤러 custom placeholder 구현 완료

--- Streetalk2.0/Controller/STWriteViewController.swift
+++ Streetalk2.0/Controller/STWriteViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class STWriteViewController: UIViewController {
+class STWriteViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var writeContentTextView: STTextView!
+    @IBOutlet weak var writtingBackgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        writeContentTextView.delegate = self
+        writeContentTextView.setPlaceholder(placeholder: "게시글 내용을 작성해주세요.")
+        
         lazy var rightButton: UIBarButtonItem = {
             let button = UIBarButtonItem(title: "등록", style: .plain, target: self, action: #selector(writeButtonTapped(_:)))
             button.tintColor = .streetalkPink
@@ -19,6 +25,26 @@ class STWriteViewController: UIViewController {
         }()
         
         self.navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        let contentTextView = textView as! STTextView
+        if contentTextView.text == "" {
+            contentTextView.text = contentTextView.placeholder
+            contentTextView.textColor = .placeholderText
+            self.writtingBackgroundImageView.isHidden = false
+        } else {
+            self.writtingBackgroundImageView.isHidden = true
+        }
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.writtingBackgroundImageView.isHidden = true
+        let contentTextView = textView as! STTextView
+        if contentTextView.text == contentTextView.placeholder {
+            contentTextView.text = ""
+            contentTextView.textColor = .label
+        }
     }
     
     @objc func writeButtonTapped(_ sender: UIButton) {
