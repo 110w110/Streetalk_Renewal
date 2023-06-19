dc5943552c8abd71185c8b30cff0ff02b44a60bd
110w110 <110w110.official@gmail.com>
2023-06-19 13:44:35 +0900

    Join 로직 중 휴대폰 번호를 입력할 때 적절히 포맷팅하는 로직과 유효한 번호일 때 버튼이 활성화되는 로직 추가

--- Streetalk2.0/Controller/STJoinMobileAuthViewController.swift
+++ Streetalk2.0/Controller/STJoinMobileAuthViewController.swift
@@ -6,21 +6,42 @@
 //
 
 import UIKit
+import AnyFormatKit
 
 class STJoinMobileAuthViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var mobileNumberTextField: UITextField!
+    @IBOutlet weak var submitButton: STButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        submitButton.isEnabled = false
 
         mobileNumberTextField.delegate = self
         mobileNumberTextField.becomeFirstResponder()
     }
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // TODO: 번호 체크하는 로직 추가하기
+        // 번호 입력에 따라 실시간으로 포맷팅
+        guard let text = textField.text else {
+            return false
+        }
+        let characterSet = CharacterSet(charactersIn: string)
+        if CharacterSet.decimalDigits.isSuperset(of: characterSet) == false {
+            return false
+        }
+
+        let formatter = DefaultTextInputFormatter(textPattern: "###-####-####")
+        let result = formatter.formatInput(currentText: text, range: range, replacementString: string)
+        textField.text = result.formattedText
+        let position = textField.position(from: textField.beginningOfDocument, offset: result.caretBeginOffset)!
+        textField.selectedTextRange = textField.textRange(from: position, to: position)
         
-        return true
+        // 유효한 번호가 입력되었을 때 버튼이 활성화됨
+        if let string = textField.text {
+            submitButton.isEnabled = string.isValidPhoneNum()
+        }
+        
+        return false
     }
 }
---
+++ Streetalk2.0/Extension/String+.swift
@@ -0,0 +1,14 @@
+//
+//  String+.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/06/19.
+//
+
+import Foundation
+
+extension String {
+    func isValidPhoneNum() -> Bool {
+        return NSPredicate(format:"SELF MATCHES %@", "^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$").evaluate(with: self)
+    }
+}
