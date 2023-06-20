d8b2c7d52c38e1956ca6a90e4d673b7926beda52
110w110 <110w110.official@gmail.com>
2023-06-20 15:37:45 +0900

    setRoundedBorder에 shadow 효과를 선택적으로 넣을 수 있게 변경 (기본값은 none)

--- Streetalk2.0/Extension/UIView+.swift
+++ Streetalk2.0/Extension/UIView+.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 extension UIView {
-    func setRoundedBorder() {
+    func setRoundedBorder(shadow: Bool = false) {
         self.layer.borderColor = UIColor.systemGray5.cgColor
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 10
+        self.backgroundColor = .systemBackground
+        
+        if (shadow) {
+            self.layer.shadowOffset = CGSize(width: 0, height: 10)
+            self.layer.shadowOpacity = 0.15
+            self.layer.shadowRadius = 15
+        }
     }
 }
