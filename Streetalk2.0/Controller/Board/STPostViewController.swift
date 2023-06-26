3155882100ec59d520a19f668f4588121d7b1359
110w110 <110w110.official@gmail.com>
2023-06-26 14:39:26 +0900

    Post 게시글의 STPostContentsCollectionViewCell 구현

---
+++ Streetalk2.0/Controller/Board/STPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  STPostViewController.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/06/26.
+//
+
+import UIKit
+
+class STPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Streetalk2.0/View/STPostContentsCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  STPostContentsCollectionViewCell.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/06/26.
+//
+
+import UIKit
+
+class STPostContentsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var contentsViewBackground: UIStackView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var postContentsLabel: UILabel!
+    @IBOutlet var commentImageView: UIImageView!
+    @IBOutlet var commentCountLabel: UILabel!
+    @IBOutlet var likeIamgeView: UIImageView!
+    @IBOutlet var likeCountLabel: UILabel!
+    @IBOutlet var scrapImageView: UIImageView!
+    @IBOutlet var scrapCountLabel: UILabel!
+    
+}
