e611b1ac09f17531cd86af9161d15fa252a01df7
110w110 <110w110.official@gmail.com>
2023-06-27 11:57:51 +0900

    post 컬렉션뷰 cell 등록

--- Streetalk2.0/Controller/Board/STPostViewController.swift
+++ Streetalk2.0/Controller/Board/STPostViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class STPostViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension STPostViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCell", for: indexPath) as! STPostContentsCollectionViewCell
+        return cell
+    }
+    
 }
