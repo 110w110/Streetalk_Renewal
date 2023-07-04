8aafa4f594c70db3059fbe2f86fe38777b39c412
110w110 <110w110.official@gmail.com>
2023-07-04 18:41:40 +0900

    collectionView 제거로 인한 코드 정리

--- Streetalk2.0/Controller/Board/STPostViewController.swift
+++ Streetalk2.0/Controller/Board/STPostViewController.swift
@@ -10,12 +10,10 @@ import UIKit
 class STPostViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -53,32 +51,4 @@ extension STPostViewController: UITableViewDelegate {
         
     }
     
-}
-
-extension STPostViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCell", for: indexPath) as! STPostContentsCollectionViewCell
-        cell.setRoundedBorder(shadow: true)
-        return cell
-    }
-    
-}
-
-extension STPostViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCell", for: indexPath) as! STPostContentsCollectionViewCell
-        let width: CGFloat = collectionView.frame.width - 20
-        
-        let boundingRect = NSString(string: cell.postContentsLabel.text ?? "temp").boundingRect(with: CGSize(width: width - 60, height: CGFloat.greatestFiniteMagnitude),
-                                                options: .usesLineFragmentOrigin,
-                                                attributes: [.font: UIFont.systemFont(ofSize: 14, weight: .regular)],
-                                                context: nil)
-
-        return CGSize(width: width, height: boundingRect.height + 160)
-    }
 }
