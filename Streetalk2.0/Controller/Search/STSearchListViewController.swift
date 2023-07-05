de4b8b95a3eed0420e189d4318c764722c0aa614
110w110 <110w110.official@gmail.com>
2023-07-06 02:32:23 +0900

    Search 구현

--- Streetalk2.0/Controller/Search/STSearchListViewController.swift
+++ Streetalk2.0/Controller/Search/STSearchListViewController.swift
@@ -9,21 +9,66 @@ import UIKit
 
 class STSearchListViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchTextField: UITextField!
+    
+    private var postList: [SearchPost] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func searchTextFieldEditing(_ sender: Any) {
+        guard let text = self.searchTextField.text else { return }
+        print(text)
+        let request = SearchRequest(additionalInfo: text)
+        request.request(completion: { result in
+            switch result {
+            case let .success(data):
+                print(data)
+                self.postList = data
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case let .failure(error):
+                print(error)
+            }
+        })
     }
-    */
+    
+}
 
+
+extension STSearchListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postTableViewCell", for: indexPath) as! STPostTableViewCell
+        cell.selectionStyle = .none
+        cell.titleLabel.text = postList[indexPath.row].title
+        cell.contentLabel.text = postList[indexPath.row].content
+//        cell. = postList[indexPath.row].
+        return cell
+    }
+    
+}
+
+extension STSearchListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // indexPath에 따라 정보 넘겨야함
+        let storyboard = UIStoryboard(name: "Board", bundle: nil)
+        let postViewController = storyboard.instantiateViewController(withIdentifier: "postViewController") as! STPostViewController
+        postViewController.title = "지역 게시판"
+        postViewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(postViewController, animated: true)
+    }
+    
 }
---
+++ Streetalk2.0/Model/SearchPost.swift
@@ -0,0 +1,65 @@
+//
+//  SearchPost.swift
+//  Streetalk2.0
+//
+//  Created by 한태희 on 2023/07/06.
+//
+
+import Foundation
+
+struct SearchPost: Codable {
+    let createdDate : String?
+    let modifiedDate : String?
+    let id : Int?
+    let title : String?
+    let content : String?
+    let likeCount : Int?
+    let scrapCount : Int?
+    let replyCount : Int?
+    let images : [SearchImage]?
+    let replyList : [SearchReply]?
+
+    enum CodingKeys: String, CodingKey {
+        case createdDate
+        case modifiedDate
+        case id
+        case title
+        case content
+        case likeCount
+        case scrapCount
+        case replyCount
+        case images
+        case replyList
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        createdDate = try values.decodeIfPresent(String.self, forKey: .createdDate)
+        modifiedDate = try values.decodeIfPresent(String.self, forKey: .modifiedDate)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        title = try values.decodeIfPresent(String.self, forKey: .title)
+        content = try values.decodeIfPresent(String.self, forKey: .content)
+        likeCount = try values.decodeIfPresent(Int.self, forKey: .likeCount)
+        scrapCount = try values.decodeIfPresent(Int.self, forKey: .scrapCount)
+        replyCount = try values.decodeIfPresent(Int.self, forKey: .replyCount)
+        images = try values.decodeIfPresent([SearchImage].self, forKey: .images)
+        replyList = try values.decodeIfPresent([SearchReply].self, forKey: .replyList)
+    }
+
+}
+
+struct SearchReply : Codable {
+    let createdDate : String?
+    let modifiedDate : Date?
+    let id : Int?
+    let nickName : String?
+    let location : String?
+    let content : String?
+}
+
+struct SearchImage : Codable {
+    let createdDate : String?
+    let modifiedDate : String?
+    let id : Int?
+    let name : String?
+}
