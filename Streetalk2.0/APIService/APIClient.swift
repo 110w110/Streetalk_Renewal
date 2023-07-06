af61057afdaa755d01387d6c272fc6c475522b33
110w110 <110w110.official@gmail.com>
2023-07-06 14:33:28 +0900

    URL 생성 시 인코딩 처리 구현

--- Streetalk2.0/APIService/APIClient.swift
+++ Streetalk2.0/APIService/APIClient.swift
@@ -14,7 +14,7 @@ final class APIClient {
     private let timeoutInterval = 10.0
     
     func request(url: String, method: HttpMethods, header: [String : String],  param: [String : Any]?, completion: @escaping (Result<Data, APIError>) -> Void) {
-        guard let url = URL(string: url) else {
+        guard let url = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: url) else {
             print("Error: cannot create URL")
 //            throw APIError.invalidUrl
             return
