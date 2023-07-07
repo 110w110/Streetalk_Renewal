77309fefbfdb060b1d3e74aa0aa40b3b9a0f9ce8
110w110 <110w110.official@gmail.com>
2023-07-07 16:09:33 +0900

    boardList UI 구현

--- Streetalk2.0/Controller/Board/STBoardListViewController.swift
+++ Streetalk2.0/Controller/Board/STBoardListViewController.swift
@@ -14,12 +14,16 @@ class STBoardListViewController: UIViewController {
     @IBOutlet var mainBoardListCollectionView: UICollectionView!
     @IBOutlet var subBoardListCollectionView: UICollectionView!
     
+    let myBoardImageList = [UIImage(named: "MyPost"), UIImage(named: "MyComment"), UIImage(named: "MyLike"), UIImage(named: "MyScrap")]
+    let debugMyBoardTitles = ["내 게시글", "내 댓글", "추천한 게시글", "내 스크랩"]
     let debugBoardTitles = ["인기 게시글", "자유 게시판", "지역 게시판", "업종 게시판"]
     let debugSubBoardTitles = ["주식 게시판", "해외축구 게시판", "게임 게시판", "자전거 게시판"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        myBoardListCollectionView.dataSource = self
+        myBoardListCollectionView.delegate = self
         mainBoardListCollectionView.dataSource = self
         mainBoardListCollectionView.delegate = self
         subBoardListCollectionView.dataSource = self
@@ -45,6 +49,42 @@ extension STBoardListViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == myBoardListCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myBoardListCell", for: indexPath)
+            
+            let imageView = {
+                let view = UIImageView(image: myBoardImageList[indexPath.row]?.withAlignmentRectInsets(UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)))
+                view.contentMode = .scaleAspectFit
+                return view
+            }()
+            
+            let label = {
+                let label = UILabel()
+                label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+                label.text = debugMyBoardTitles[indexPath.row]
+                label.textAlignment = .center
+                return label
+            }()
+            
+            cell.addSubview(imageView)
+            cell.addSubview(label)
+            
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.leadingAnchor.constraint(equalTo: cell.leadingAnchor, constant: 0).isActive = true
+            imageView.trailingAnchor.constraint(equalTo: cell.trailingAnchor, constant: 0).isActive = true
+            imageView.topAnchor.constraint(equalTo: cell.topAnchor, constant: 0).isActive = true
+            imageView.bottomAnchor.constraint(equalTo: label.topAnchor, constant: 0).isActive = true
+            
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.leadingAnchor.constraint(equalTo: cell.leadingAnchor, constant: 0).isActive = true
+            label.trailingAnchor.constraint(equalTo: cell.trailingAnchor, constant: 0).isActive = true
+            label.bottomAnchor.constraint(equalTo: cell.bottomAnchor, constant: 0).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 15).isActive = true
+            
+            return cell
+        }
+        
         let cell = {
             if collectionView == mainBoardListCollectionView {
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "boardCollectionViewCell", for: indexPath)
@@ -85,6 +125,10 @@ extension STBoardListViewController: UICollectionViewDelegateFlowLayout {
         if collectionView == mainBoardListCollectionView || collectionView == subBoardListCollectionView {
             let width: CGFloat = collectionView.frame.width
             let height: CGFloat = collectionView.frame.height / 4
+            return CGSize(width: width, height: height )
+        } else if collectionView == myBoardListCollectionView {
+            let width: CGFloat = collectionView.frame.width / 4
+            let height: CGFloat = collectionView.frame.height - 20
             return CGSize(width: width, height: height )
         }
         return CGSize(width: 0.0, height: 0.0 )
