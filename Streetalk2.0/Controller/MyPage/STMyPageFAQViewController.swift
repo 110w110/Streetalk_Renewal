2bdb9d8235f8636b462383c3ad460116acc3433d
110w110 <110w110.official@gmail.com>
2023-07-11 17:21:27 +0900

    문의하기 email 연결

--- Streetalk2.0/Controller/MyPage/STMyPageFAQViewController.swift
+++ Streetalk2.0/Controller/MyPage/STMyPageFAQViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import MessageUI
 
 class STMyPageFAQViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        textView.text = "\n문의 사항은 아래의 메일로 주시면 검토 후 연락 드리겠습니다.\n(dev.hantae@gmail.com)"
+        textView.delegate = self
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.dataDetectorTypes = .link
+    }
+
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "전송 불가", message: "이메일 앱 연결을 확인해주세요", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default)
+        sendMailErrorAlert.addAction(confirmAction)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    // Device Identifier 찾기
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
 
+    // 현재 버전 가져오기
+    func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+}
+
+extension STMyPageFAQViewController: UITextViewDelegate, MFMailComposeViewControllerDelegate {
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        if MFMailComposeViewController.canSendMail() {
+            let compseVC = MFMailComposeViewController()
+            compseVC.mailComposeDelegate = self
+            compseVC.setToRecipients(["dev.hantae@gmail.com"])
+            compseVC.setSubject("Streetalk Asking Mail")
+            let body = """
+                         -------------------
+                         
+                         Device Model : \(self.getDeviceIdentifier())
+                         Device OS : \(UIDevice.current.systemVersion)
+                         App Version : \(self.getCurrentVersion())
+                         
+                         -------------------
+                         
+                         아래에 문의하실 내용에 대해 작성해주세요.
+                         
+                         
+                         """
+            compseVC.setMessageBody(body, isHTML: false)
+            self.present(compseVC, animated: true, completion: nil)
+        }
+        else {
+            self.showSendMailErrorAlert()
+        }
+        return false
+    }
 }
