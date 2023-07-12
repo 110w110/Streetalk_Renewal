22a0a9155afb2ffbf7fc8c7968048a76220d7f35
110w110 <110w110.official@gmail.com>
2023-07-12 13:51:47 +0900

    게시판 리스트 및 게시판에서 Like 와 Scrap 터치 이벤트 연결

--- Streetalk2.0/View/STPostTableViewCell.swift
+++ Streetalk2.0/View/STPostTableViewCell.swift
@@ -22,6 +22,9 @@ class STPostTableViewCell: UITableViewCell {
     @IBOutlet var commentCount: UILabel!
     @IBOutlet var scrapCount: UILabel!
     
+    @IBOutlet var likeView: UIStackView!
+    @IBOutlet var scrapView: UIStackView!
+    
     var postId: Int?
     
     override func awakeFromNib() {
@@ -30,6 +33,15 @@ class STPostTableViewCell: UITableViewCell {
         cellBackground.setRoundedBorder()
         bottomStackView.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 15, right: 10)
         bottomStackView.isLayoutMarginsRelativeArrangement = true
+        
+        let like = UITapGestureRecognizer(target: self, action: #selector(likeTapped))
+        likeView.isUserInteractionEnabled = true
+        likeView.addGestureRecognizer(like)
+        
+        let scrap = UITapGestureRecognizer(target: self, action: #selector(scrapTapped))
+        scrapView.isUserInteractionEnabled = true
+        scrapView.addGestureRecognizer(scrap)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -38,4 +50,15 @@ class STPostTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension STPostTableViewCell {
+    
+    @objc func likeTapped(sender: UITapGestureRecognizer) {
+        print("like")
+    }
+    
+    @objc func scrapTapped(sender: UITapGestureRecognizer) {
+        print("scrap")
+    }
 }
