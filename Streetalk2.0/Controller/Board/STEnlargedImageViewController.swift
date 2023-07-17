f0b8c73970966b7ab9080b4af0f36349cc66248e
110w110 <110w110.official@gmail.com>
2023-07-18 03:19:46 +0900

    Pull down to dismiss 구현으로 쉬운 사진 닫기 추가

--- Streetalk2.0/Controller/Board/STEnlargedImageViewController.swift
+++ Streetalk2.0/Controller/Board/STEnlargedImageViewController.swift
@@ -14,6 +14,8 @@ class STEnlargedImageViewController: UIViewController {
     
     var imageUrl: String?
     
+    private var originalPosition: CGPoint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,10 +34,46 @@ class STEnlargedImageViewController: UIViewController {
         scrollView.zoomScale = 1.0
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 3.0
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(_:)))
+        self.view.addGestureRecognizer(panGestureRecognizer)
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
         self.dismiss(animated: true)
+    }
+    
+    // 참고 : https://ios-development.tistory.com/885
+    @objc func panGestureAction(_ panGesture: UIPanGestureRecognizer) {
+        switch panGesture.state {
+        case .began:
+            self.originalPosition = view.center
+        case .changed:
+            let translation = panGesture.translation(in: view)
+            self.view.frame.origin = CGPoint(x: translation.x, y: translation.y)
+        case .ended:
+            guard let originalPosition = self.originalPosition else { return }
+            let velocity = panGesture.velocity(in: view)
+            guard velocity.y >= 1500 else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.view.center = originalPosition
+                })
+                return
+            }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                self.view.frame.origin = CGPoint(
+                    x: self.view.frame.origin.x,
+                    y: self.view.frame.size.height
+                )},
+                           completion: { (isCompleted) in
+                if isCompleted {
+                    self.dismiss(animated: false, completion: nil)
+                }
+            })
+          default:
+            return
+        }
     }
 }
 
